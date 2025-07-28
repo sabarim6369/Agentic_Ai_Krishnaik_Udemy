2ad0eb4c20ed9dc2ef586184a5dcf03fcbbc586a
sabarim6369 <sabarim6369@gmail.com>
2025-07-28 16:54:46 +0530

    learned recursivecharsplitter csvspliter jsonsplitter

--- uv_project/dummy.py
+++ uv_project/dummy.py
@@ -12,6 +12,9 @@
 # print(docs_web[0].page_content) 
 from langchain_community.document_loaders import TextLoader, PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_text_splitters import HTMLHeaderTextSplitter
+
 
 # Load text and PDF files
 loader = TextLoader("file.txt")
@@ -28,9 +31,39 @@ text_splitter = RecursiveCharacterTextSplitter(
     chunk_size=500,       # Adjust size as needed
     chunk_overlap=100     # Slight overlap for context continuity
 )
-split_docs = text_splitter.split_documents(all_docs)
+textsplitterfromcharactertextsplitter=CharacterTextSplitter(
+    separator="\n",
+    chunk_size=500,
+    chunk_overlap=100
+)
+htmlheadersplitter=HTMLHeaderTextSplitter(
+    headers_to_split_on=[
+        ("h1", "Header 1"),
+        ("h2", "Header 2"),
+        ("h3", "Header 3"),
+    ]
+)
 
-for i,doc in enumerate(split_docs):
+split_docs = text_splitter.split_documents(all_docs)
+split=textsplitterfromcharactertextsplitter.split_documents(all_docs)
+split_html=htmlheadersplitter.split_documents(all_docs)
+# for i,doc in enumerate(split_docs):
+#     print(f"Document {i+1}:")
+#     print(doc.page_content)
+#     print("-" * 40)  # Separator for clarity
+
+print(split);
+for i,doc in enumerate(split_html):
     print(f"Document {i+1}:")
     print(doc.page_content)
     print("-" * 40)  # Separator for clarity
+
+
+
+# RecursiveJsonSplitter   ->for JSON files
+# RecursiveCharacterTextSplitter   ->for text files
+# CharacterTextSplitter   ->for text files
+# HTMLHeaderTextSplitter   ->for HTML files
+# CSVHeaderTextSplitter   ->for CSV files
+# RecursiveMarkdownTextSplitter   ->for Markdown files
+# RecursiveCodeTextSplitter   ->for code files
